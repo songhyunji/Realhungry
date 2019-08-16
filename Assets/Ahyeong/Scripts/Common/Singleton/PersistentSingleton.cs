c61df3799fb404d3244bd682c207b4fabdaa035c
devAdaid <ahyeong2533@gmail.com>
2019-08-16 20:32:05 +0900

    개인 프로젝트 세팅
    프로토타이핑을 위한 폴더 및 파일 등 세팅 (Ahyeong 하위 파일)
    기본 스크립트 구성

---
+++ Assets/Ahyeong/Scripts/Common/Singleton/PersistentSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PersistentSingleton<T> : MonoSingleton<T> where T : MonoBehaviour
+{
+    protected void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else if (_instance != this)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Ahyeong/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : PersistentSingleton<SoundManager>
+{
+    public float BgmVolume
+    {
+        get
+        {
+            _bgmVolume = PlayerPrefs.GetFloat("Volume_Bgm", 0.5f);
+            return _bgmVolume;
+        }
+
+        set
+        {
+            _bgmVolume = value;
+            bgmSource.volume = value;
+            PlayerPrefs.SetFloat("Volume_Bgm", value);
+        }
+    }
+
+    public float SfxVolume
+    {
+        get
+        {
+            _sfxVolume = PlayerPrefs.GetFloat("Volume_Sfx", 0.5f);
+            return _sfxVolume;
+        }
+
+        set
+        {
+            _sfxVolume = value;
+            sfxSource.volume = value;
+            PlayerPrefs.SetFloat("Volume_Sfx", value);
+        }
+    }
+
+    public bool BgmMute
+    {
+        get
+        {
+            _isBgmMute = PlayerPrefs.GetInt("Mute_Bgm", 0) == 1;
+            return _isBgmMute;
+        }
+
+        set
+        {
+            _isBgmMute = value;
+            bgmSource.mute = value;
+            PlayerPrefs.SetInt("Mute_Bgm", value ? 1 : 0);
+        }
+    }
+
+    public bool SfxMute
+    {
+        get
+        {
+            _isSfxMute = PlayerPrefs.GetInt("Mute_Sfx", 0) == 1;
+            return _isSfxMute;
+        }
+
+        set
+        {
+            _isSfxMute = value;
+            sfxSource.mute = value;
+            PlayerPrefs.SetInt("Mute_Sfx", value ? 1 : 0);
+        }
+    }
+
+    public AudioSource bgmSource;
+    public AudioSource sfxSource;
+
+    private bool _isBgmMute = false;
+    private bool _isSfxMute = false;
+
+    private float _bgmVolume = 0.5f;
+    private float _sfxVolume = 0.5f;
+
+    private void Start()
+    {
+        if (bgmSource == null)
+        {
+            bgmSource = gameObject.AddComponent<AudioSource>();
+            bgmSource.loop = true;
+        }
+        if (sfxSource == null)
+        {
+            sfxSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        bgmSource.volume = BgmVolume;
+        sfxSource.volume = SfxVolume;
+        bgmSource.mute = BgmMute;
+        sfxSource.mute = SfxMute;
+    }
+
+    public void PlayBgm(AudioClip clip)
+    {
+        bgmSource.clip = clip;
+        bgmSource.Play();
+    }
+
+    public void PlaySfx(AudioClip clip)
+    {
+        sfxSource.clip = clip;
+        sfxSource.Play();
+    }
+
+    public void PlaySfxOneShot(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void StopBgm()
+    {
+        bgmSource.Stop();
+    }
+}
