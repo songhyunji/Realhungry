630da8efa441e99baef57edb72cd120f2c35bb02
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-08-17 14:23:42 +0900

    add effector
    이펙터 추가
    에셋 추가

---
+++ Assets/HyunJi/Script/RandomSpawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+	public GameObject[] ingredients = new GameObject[10];
+	int ing_num;
+	float posX, posY;
+	float scale = 1;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(DecScale());
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		ing_num = Random.Range(0, 5);
+		posX = Random.Range(-10f, 10f);
+		posY = Random.Range(0, 5f);
+
+		StartCoroutine(Spawn());
+	}
+
+	IEnumerator Spawn()
+	{
+		GameObject instant = Instantiate(ingredients[ing_num], new Vector3(posX, posY, 0), Quaternion.identity);
+		instant.transform.localScale = new Vector3(scale, scale, scale);
+		instant.transform.parent = gameObject.transform;
+		yield return new WaitForSecondsRealtime(2.0f);
+		Destroy(instant);
+	}
+
+	IEnumerator DecScale()
+	{
+		if(scale > 0)
+		{
+			yield return new WaitForSecondsRealtime(1.0f);
+			scale -= 0.2f;
+			StartCoroutine(DecScale());
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+}
